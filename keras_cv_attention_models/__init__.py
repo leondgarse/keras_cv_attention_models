ea299be1e3d9ae830736a9b31d3a3ca11d98520a
leondgarse <leondgarse@gmail.com>
2021-09-01 16:28:00 +0800

    remove imagenet import

--- keras_cv_attention_models/__init__.py
+++ keras_cv_attention_models/__init__.py
@@ -18,4 +18,3 @@ from keras_cv_attention_models.resnet_family import  resnet_quad
 from keras_cv_attention_models.resnet_family import  resnet_deep
 from keras_cv_attention_models import volo
 from keras_cv_attention_models import download_and_load
-from keras_cv_attention_models import imagenet
