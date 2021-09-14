e1bc36774853471427c26ec84e5deaccef8b04a0
leondgarse <leondgarse@gmail.com>
2021-09-14 11:02:44 +0800

    reformat setup.py

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-exec(open('keras_cv_attention_models/version.py').read())
+exec(open("keras_cv_attention_models/version.py").read())
 setup(
     name="keras-cv-attention-models",
     version=__version__,
-    description="tensorflow keras computer vision  attention models",
+    description="Tensorflow keras computer vision  attention models",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url="https://github.com/leondgarse/keras_cv_attention_models",
     author="Leondgarse",
     author_email="leondgarse@google.com",
@@ -25,25 +25,24 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-
     # Note that this is a string of words separated by whitespace, not a list.
-    keywords='tensorflow keras cv attension pretrained models',
-    packages=find_packages(exclude=['tests']),
+    keywords="tensorflow keras cv attension pretrained models",
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=["tensorflow", "tensorflow-addons", "einops"],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     license="Apache 2.0",
 )
