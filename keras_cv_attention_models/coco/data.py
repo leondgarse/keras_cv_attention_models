e0fc995daaa5230110c3f6f0c29ed51e3443c49a
leondgarse <leondgarse@gmail.com>
2022-03-26 17:14:11 +0800

    fix invalid coco 90 labels

--- keras_cv_attention_models/coco/data.py
+++ keras_cv_attention_models/coco/data.py
@@ -11,7 +11,7 @@ COCO_LABELS = """person, bicycle, car, motorcycle, airplane, bus, train, truck, 
     cake, chair, couch, potted plant, bed, dining table, toilet, tv, laptop, mouse, remote, keyboard, cell phone, microwave, oven,
     toaster, sink, refrigerator, book, clock, vase, scissors, teddy bear, hair drier, toothbrush"""
 COCO_80_LABEL_DICT = {id: ii.strip() for id, ii in enumerate(COCO_LABELS.split(","))}
-INVALID_ID_90 = [12, 26, 29, 30, 45, 66, 68, 69, 71, 83]
+INVALID_ID_90 = [11, 25, 28, 29, 44, 65, 67, 68, 70, 82]
 COCO_90_LABEL_DICT = {id: ii for id, ii in zip(set(range(90)) - set(INVALID_ID_90), COCO_80_LABEL_DICT.values())}
 COCO_90_LABEL_DICT.update({ii: "Unknown" for ii in INVALID_ID_90})
 COCO_80_to_90_LABEL_DICT = {id_80: id_90 for id_80, id_90 in enumerate(set(range(90)) - set(INVALID_ID_90))}
