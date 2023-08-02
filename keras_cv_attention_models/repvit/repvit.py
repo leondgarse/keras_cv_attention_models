5f2edd2fce1af953970facb8c3c50b0da598733c
leondgarse <leondgarse@gmail.com>
2023-08-02 23:17:19 +0800

    add a deploy mode for repvit

--- keras_cv_attention_models/repvit/repvit.py
+++ keras_cv_attention_models/repvit/repvit.py
@@ -4,6 +4,7 @@ from keras_cv_attention_models import backend
 from keras_cv_attention_models.backend import layers, functional, models, image_data_format
 from keras_cv_attention_models.models import register_model
 from keras_cv_attention_models.attention_layers import (
+    activation_by_name,
     conv2d_no_bias,
     depthwise_conv2d_no_bias,
     batchnorm_with_activation,
@@ -23,7 +24,10 @@ PRETRAINED_DICT = {
 }
 
 
-def rep_vgg_depthwise(inputs, kernel_size=3, strides=1, name=""):
+def rep_vgg_depthwise(inputs, kernel_size=3, strides=1, deploy=False, name=""):
+    if deploy:
+        return depthwise_conv2d_no_bias(inputs, kernel_size=kernel_size, strides=strides, use_bias=True, padding="same", name=name + "REPARAM_1_")
+
     dw_1 = depthwise_conv2d_no_bias(inputs, kernel_size=kernel_size, strides=strides, padding="same", name=name + "REPARAM_1_")
     dw_1 = batchnorm_with_activation(dw_1, epsilon=BATCH_NORM_EPSILON, activation=None, name=name + "REPARAM_1_")
     dw_2 = depthwise_conv2d_no_bias(inputs, 1, name=name + "REPARAM_2_")
@@ -31,19 +35,22 @@ def rep_vgg_depthwise(inputs, kernel_size=3, strides=1, name=""):
     return layers.Add(name=name + "REPARAM_out")([dw_1, dw_2, inputs])
 
 
-def dwconv_bn_conv_bn(inputs, out_channel, kernel_size=1, strides=1, name=""):
-    nn = depthwise_conv2d_no_bias(inputs, kernel_size, strides=strides, padding="same", name=name + "1_")
-    nn = batchnorm_with_activation(nn, epsilon=BATCH_NORM_EPSILON, name=name + "1_")
-    nn = conv2d_no_bias(nn, out_channel, name=name + "2_")
-    nn = batchnorm_with_activation(nn, epsilon=BATCH_NORM_EPSILON, activation=None, name=name + "2_")
+def dwconv_bn_conv_bn(inputs, out_channel, kernel_size=1, strides=1, deploy=False, name=""):
+    nn = depthwise_conv2d_no_bias(inputs, kernel_size, strides=strides, use_bias=deploy, padding="same", name=name + "1_")
+    nn = nn if deploy else batchnorm_with_activation(nn, epsilon=BATCH_NORM_EPSILON, name=name + "1_")
+    nn = conv2d_no_bias(nn, out_channel, use_bias=deploy, name=name + "2_")
+    nn = nn if deploy else batchnorm_with_activation(nn, epsilon=BATCH_NORM_EPSILON, activation=None, name=name + "2_")
     return nn
 
 
-def conv_bn_act_conv_bn(inputs, hidden_channel, out_channel, use_residual=True, kernel_size=1, strides=1, drop_rate=0, activation="gelu", name=""):
-    nn = conv2d_no_bias(inputs, hidden_channel, kernel_size, strides=strides, padding="same", name=name + "1_")
-    nn = batchnorm_with_activation(nn, epsilon=BATCH_NORM_EPSILON, activation=activation, name=name + "1_")
-    nn = conv2d_no_bias(nn, out_channel, kernel_size, strides=strides, padding="same", name=name + "2_")
-    nn = batchnorm_with_activation(nn, epsilon=BATCH_NORM_EPSILON, activation=None, name=name + "2_")
+def conv_bn_act_conv_bn(
+    inputs, hidden_channel, out_channel, use_residual=True, kernel_size=1, strides=1, deploy=False, drop_rate=0, activation="gelu", name=""
+):
+    nn = conv2d_no_bias(inputs, hidden_channel, kernel_size, strides=strides, use_bias=deploy, padding="same", name=name + "1_")
+    nn = nn if deploy else batchnorm_with_activation(nn, epsilon=BATCH_NORM_EPSILON, activation=None, name=name + "1_")
+    nn = activation_by_name(nn, activation=activation, name=name + "1_")
+    nn = conv2d_no_bias(nn, out_channel, kernel_size, strides=strides, use_bias=deploy, padding="same", name=name + "2_")
+    nn = nn if deploy else batchnorm_with_activation(nn, epsilon=BATCH_NORM_EPSILON, activation=None, name=name + "2_")
     nn = drop_block(nn, drop_rate=drop_rate, name=name)
     return (nn + inputs) if use_residual else nn
 
@@ -54,6 +61,7 @@ def RepViT(
     stem_width=-1,
     se_ratio=0.25,  # will use `se_module` every other block in each stack if > 0
     input_shape=(224, 224, 3),
+    deploy=False,  # For test
     num_classes=1000,
     activation="gelu",
     drop_connect_rate=0,
@@ -70,7 +78,9 @@ def RepViT(
     input_shape = backend.align_input_shape_by_image_data_format(input_shape)
     inputs = layers.Input(input_shape)
     stem_width = stem_width if stem_width > 0 else out_channels[0]
-    nn = conv_bn_act_conv_bn(inputs, stem_width // 2, stem_width, use_residual=False, kernel_size=3, strides=2, activation=activation, name="stem_")
+    nn = conv_bn_act_conv_bn(
+        inputs, stem_width // 2, stem_width, use_residual=False, kernel_size=3, strides=2, deploy=deploy, activation=activation, name="stem_"
+    )
 
     """ stage [1, 2, 3, 4] """
     total_blocks = sum(num_blocks)
@@ -79,20 +89,25 @@ def RepViT(
         out_channel = make_divisible(out_channel, 8)
         stack_name = "stack{}_".format(stack_id + 1)
         if stack_id > 0:
-            nn = dwconv_bn_conv_bn(nn, out_channel, kernel_size=3, strides=2, name=stack_name + "downsample_token_mixer_")
-            nn = conv_bn_act_conv_bn(nn, out_channel * 2, out_channel, activation=activation, name=stack_name + "downsample_channel_mixer_")
+            nn = dwconv_bn_conv_bn(nn, out_channel, kernel_size=3, strides=2, deploy=deploy, name=stack_name + "downsample_token_mixer_")
+            nn = conv_bn_act_conv_bn(nn, out_channel * 2, out_channel, deploy=deploy, activation=activation, name=stack_name + "downsample_channel_mixer_")
 
         for block_id in range(num_block):
             name = "stack{}_block{}_".format(stack_id + 1, block_id + 1)
             block_drop_rate = drop_connect_rate * global_block_id / total_blocks
             cur_se_ratio = 0 if block_id % 2 == 1 or block_id == (num_block - 1) else se_ratio
             # print(f"{name = }, {cur_se_ratio = }")
-            nn = rep_vgg_depthwise(nn, name=name + "repvgg_")
+            nn = rep_vgg_depthwise(nn, deploy=deploy, name=name + "repvgg_")
             nn = se_module(nn, cur_se_ratio, activation="relu", name=name + "se_") if cur_se_ratio > 0 else nn
-            nn = conv_bn_act_conv_bn(nn, out_channel * 2, out_channel, activation=activation, drop_rate=block_drop_rate, name=name + "channel_mixer_")
+            nn = conv_bn_act_conv_bn(
+                nn, out_channel * 2, out_channel, deploy=deploy, activation=activation, drop_rate=block_drop_rate, name=name + "channel_mixer_"
+            )
             global_block_id += 1
 
-    if num_classes > 0:
+    if deploy and num_classes > 0:
+        nn = layers.GlobalAveragePooling2D()(nn)
+        out = layers.Dense(num_classes, dtype="float32", activation=classifier_activation, name="head")(nn)
+    elif num_classes > 0:
         nn = layers.GlobalAveragePooling2D(keepdims=True)(nn)
         out = batchnorm_with_activation(nn, activation=None, name="head_")
         out = layers.Flatten()(out)
